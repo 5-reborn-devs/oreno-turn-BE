721d345b6005ec0f768e281032877c7779d9fcd9
skybanana <112135946+skybanana@users.noreply.github.com>
2024-11-16 19:25:25 +0900

    add: room, client session

---
+++ src/handlers/room/room.create.handler.js
@@ -0,0 +1,8 @@
+import { getProtoMessages } from '../../init/loadProto';
+
+export const createRoomHandler = () => {
+  const protoMessages = getProtoMessages();
+  const roomData = protoMessages.common.roomData;
+
+  return;
+};
