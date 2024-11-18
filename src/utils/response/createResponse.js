12d97138ca687a2fd6ce9eca142c6a8aa3410a90
skybanana <112135946+skybanana@users.noreply.github.com>
2024-11-18 15:15:31 +0900

    fix: createResponse

--- src/utils/response/createResponse.js
+++ src/utils/response/createResponse.js
@@ -2,7 +2,7 @@ import { PACKET_NUMBER } from '../../constants/header.js';
 import { getProtoMessages } from '../../init/loadProto.js';
 import { serializer } from '../serilaizer.js';
 
-const sendResponsePacket = (socket, packetType, responseMessage) => {
+const sendResponsePacket = (socket, responseMessage) => {
   try {
     const protoMessages = getProtoMessages();
     const GamePacket = protoMessages.gamePacket.GamePacket;
@@ -11,12 +11,13 @@ const sendResponsePacket = (socket, packetType, responseMessage) => {
     const gamePacketBuffer = GamePacket.encode(responseGamePacket).finish();
 
     // 정규화 과정을 통해 패킷 제작
-    const serializedPacket = serializer(gamePacketBuffer, packetType);
+    const key = Object.keys(responseGamePacket)[0];
+    const serializedPacket = serializer(gamePacketBuffer, PACKET_NUMBER[key]);
 
     //클라이언트에게 패킷 전송
     socket.write(serializedPacket);
 
-    console.log(`Sent packet of type ${PACKET_NUMBER[packetType]} to client.`);
+    console.log(`Sent packet of type ${key} to client.`);
   } catch (error) {
     console.error('Error sending response packet', error);
   }
