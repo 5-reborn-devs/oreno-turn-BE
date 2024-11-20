b7e8f03eaa4ca00350cb1624de559e4020b6f217
liha-engineer <liha.engineer@gmail.com>
2024-11-21 01:21:02 +0900

    comment: 주석 정리

--- src/handlers/index.js
+++ src/handlers/index.js
@@ -4,7 +4,8 @@ import { registerHandler } from './auth/register.handler.js';
 import { gamePrepare } from './game/gamePrepare.handler.js';
 import { gameStart } from './game/gameStart.handler.js';
 import { createRoomHandler } from './room/room.create.handler.js';
-import { fleamarketPickHandler } from './fleamarket/fleaMarket.handler.js';
+import { fleamarketPickHandler } from './fleamarket/fleamarketPick.handler.js';
+import { useCardHandler } from './card/useCard.handler.js';
 
 const handlers = {
   // 회원가입
@@ -16,18 +17,7 @@ const handlers = {
     handler: login, // 사용하게될 함수명
     protoType: 'request.C2SLoginRequest', // protobuf 타입
   },
-  // [PACKET_TYPE.USE_CARD_REQUEST]: {
-  //     handler : register, // 사용하게될 함수명
-  //     protoType : 'request.C2SCreateRoomRequest', // protobuf 타입
-  // },
-  // [PACKET_TYPE.GET_ROOM_LIST_REQUEST]: {
-  //     handler : register, // 사용하게될 함수명
-  //     protoType : 'request.C2SGetRoomListRequest', // protobuf 타입
-  // },
-  // [PACKET_TYPE.JOIN_ROOM_REQUEST]: {
-  //     handler : register, // 사용하게될 함수명
-  //     protoType : 'request.C2SJoinRandomRoomRequest', // protobuf 타입
-  // },
+
   // 게임 관련 핸들러
   [PACKET_TYPE.CREATE_ROOM_REQUEST]: {
     handler: createRoomHandler, // 사용하게될 함수명
@@ -40,6 +30,10 @@ const handlers = {
   [PACKET_TYPE.GAME_START_REQUEST]: {
     handler: gameStart, // 사용하게될 함수명
     protoType: 'request.C2SGameStartRequest',
+  },
+  [PACKET_TYPE.USE_CARD_REQUEST]: {
+    handler: useCardHandler, // 사용하게될 함수명
+    protoType: 'request.C2SUseCardRequest',
   },
   [PACKET_TYPE.FLEAMARKET_PICK_REQUEST]: {
       handler : fleamarketPickHandler, // 사용하게될 함수명
