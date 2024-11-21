1c3009af25a706f39c17a0d49c65efa447259394
yeeunsy <90146746+yeeunsy@users.noreply.github.com>
2024-11-21 20:41:00 +0900

    refactor : 로그인 코드 수정

--- src/handlers/auth/login.handler.js
+++ src/handlers/auth/login.handler.js
@@ -10,38 +10,27 @@ import { addUser, userLoggedIn } from '../../session/user.session.js';
 import { getFailCode } from '../../utils/response/failCode.js';
 
 export const loginHandler = async (socket, payload) => {
+  const { email, password } = payload;
+  const failCode = getFailCode();
+  let message;
+
+  // 패킷 데이터 전송 객체
+  let loginResponse = {
+    success: false,
+    message: '',
+    token: '',
+    myInfo: '',
+    failCode: failCode.AUTHENTICATION_FAILED,
+  };
   try {
-    const { email, password } = payload;
-    const failCode = getFailCode();
-
-    // 패킷 데이터 전송 객체
-    let loginResponse;
-
-    // 중복 로그인 확인
-    if (userLoggedIn(email)) {
-      console.log(`이미 로그인된 사용자 입니다. : ${email}`);
-      loginResponse = {
-        success: false,
-        message: `이미 로그인된 사용자 입니다. : ${email}`,
-        token: '',
-        myInfo: '',
-        failCode: failCode.AUTHENTICATION_FAILED,
-      };
-      return sendResponsePacket(socket, PACKET_TYPE.LOGIN_RESPONSE, { loginResponse, });
-    }
-
     // 아이디 검사
     const dbUser = await findUserByUserEmail(email);
+
     if (!dbUser) {
-      console.log('아이디가 틀립니다.');
-      loginResponse = {
-        success: false,
-        message: ` ${email} : 아이디가 틀립니다.`,
-        token: '',
-        myInfo: '',
-        failCode: failCode.AUTHENTICATION_FAILED,
-      };
-      return sendResponsePacket(socket, PACKET_TYPE.LOGIN_RESPONSE, { loginResponse, });
+      message = ` ${email} : 아이디가 틀립니다.`;
+      console.log(message);
+      loginResponse.message = message;
+      throw new Error(message);
     }
 
     // 비밀번호 검사
@@ -50,38 +39,41 @@ export const loginHandler = async (socket, payload) => {
     console.log('디비비번', dbUser.password);
 
     if (!isPasswordCorrect) {
-      console.log(`${password} : 비밀번호가 틀렸습니다.`);
-      loginResponse = {
-        success: false,
-        message: `비밀번호가 틀렸습니다. ${email}`,
-        token: '',
-        myInfo: '',
-        failCode: failCode.AUTHENTICATION_FAILED,
-      };
-      return sendResponsePacket(socket, PACKET_TYPE.LOGIN_RESPONSE, { loginResponse, });
+      message = `비밀번호가 틀렸습니다. ${email}`;
+      console.log(message);
+      loginResponse.message = message;
+      throw new Error(message);
     }
 
-    //토큰 발급 
-    const token = jwt.sign(dbUser, config.auth.key, { expiresIn: '12h' });
+    // 중복 로그인 확인
+    if (userLoggedIn(dbUser.userId)) {
+      message = `이미 로그인된 사용자 입니다. : ${email}`;
+      console.log(message);
+      loginResponse.message = message;
+      throw new Error(message);
+    }
 
+    //토큰 발급
+    const token = jwt.sign(dbUser, config.auth.key, { expiresIn: '12h' });
+    socket.token = token;
     //클라이언트 세션 추가
-    addClient(socket, email);
+    addClient(socket, dbUser.userId);
 
     loginResponse = {
       success: true,
       message: `로그인 성공 ! ${email}`,
       token: token,
-      myInfo: new User(email),
+      myInfo: new User(dbUser.userId, dbUser.nickname),
       failCode: failCode.NONE_FAILCODE,
     };
 
     // session에 사용자 추가
     addUser(token, loginResponse.myInfo);
-
-    return sendResponsePacket(socket, PACKET_TYPE.LOGIN_RESPONSE, { loginResponse, });
-    
   } catch (err) {
     console.log(err);
   }
-};
 
+  sendResponsePacket(socket, PACKET_TYPE.LOGIN_RESPONSE, {
+    loginResponse,
+  });
+};
