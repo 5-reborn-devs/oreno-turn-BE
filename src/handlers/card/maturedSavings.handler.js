96f7b94f8a939c1ffa49d23bb6d3aaea92d137f4
liha-engineer <liha.engineer@gmail.com>
2024-11-22 12:08:25 +0900

    refactor: 함수 인자 수정 및 내부 변수명 수정

--- src/handlers/card/maturedSavings.handler.js
+++ src/handlers/card/maturedSavings.handler.js
@@ -1,10 +1,9 @@
 import { getUserById } from '../../session/user.session.js';
 
 // 만기적금 - 은행 npc에게 사용시 핸드카드 두장을 획득한다
-export const maturedSavingsHandler = async (socket, gameDeck, cardType, targetUserId) => {
-  const user = getUserById(targetUserId);
-  const userCharacter = user.character;
-  const handCards = userCharacter.handCards;
+export const maturedSavingsHandler = async (user, gameDeck, targetUserId) => {
+  const character = user.character
+  const handCards = character.handCards;
   const earningCount = 2;
 
   // 카드더미에서 2장 뽑아 유저에게 준다 - 카드더미에서 2장 줄이고, 종류값에 맞는 카드를 유저 핸드로 넣어준다
--- src/handlers/card/winLottery.handler.js
+++ src/handlers/card/winLottery.handler.js
@@ -1,10 +1,9 @@
 import { getUserById } from "../../session/user.session.js";
 import { makeCardDeck } from "./card.js";
 
-export const winLotteryHandler = async (socket, gameDeck, cardType, targetUserId) => {
-    const user = getUserById(targetUserId);
-    const userCharacter = user.character;
-    const handCards = userCharacter.handCards;
+export const winLotteryHandler = async (user, gameDeck, targetUserId) => {
+    const character = user.character
+    const handCards = character.handCards;
     const earningCount = 3;
 
     const pickedCards = gameDeck.splice(0, earningCount);
