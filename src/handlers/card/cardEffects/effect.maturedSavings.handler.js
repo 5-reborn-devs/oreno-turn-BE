27eef5e23989b1258158eca356a42759e9265d1d
liha-engineer <liha.engineer@gmail.com>
2024-11-25 19:24:29 +0900

    refactor: 만기적금 및 복권당첨 로직 맵 객체 타입의 클래스 메서드로 수정

--- src/handlers/card/cardEffects/effect.maturedSavings.handler.js
+++ src/handlers/card/cardEffects/effect.maturedSavings.handler.js
@@ -3,15 +3,12 @@ import { getUserById } from '../../../session/user.session.js';
 // 만기적금 - 은행 npc에게 사용시 핸드카드 두장을 획득한다
 export const maturedSavingsHandler = async (user, gameDeck, targetUserId) => {
   const character = user.character
-  const handCards = character.handCards;
   const earningCount = 2;
 
   // 카드더미에서 2장 뽑아 유저에게 준다 - 카드더미에서 2장 줄이고, 종류값에 맞는 카드를 유저 핸드로 넣어준다
   const pickedCards = gameDeck.splice(0, earningCount);
-  // 얻은 카드의 타입과 장수를 handCards에 set으로 넣어준다
-  pickedCards.forEach((pickedCardType) => {
-    let count = handCards.get(pickedCardType);
-    count = !!count ? count : 0;
-    handCards.set(pickedCardType, ++count);
+  // 얻은 카드를 handCard에 추가하고 handCardCounts를 올리는 클래스 메서드를 호출ㅊ
+  pickedCards.forEach((pickedCard) => {
+    character.addCard(pickedCard)
   });
 };
--- src/handlers/card/cardEffects/effect.winLottery.handler.js
+++ src/handlers/card/cardEffects/effect.winLottery.handler.js
@@ -1,14 +1,10 @@
 export const winLotteryHandler = async (user, gameDeck, targetUserId) => {
     const character = user.character
-    const handCards = character.handCards;
     const earningCount = 3;
 
     const pickedCards = gameDeck.splice(0, earningCount);
     // 얻은 카드의 타입과 장수를 handCards에 set으로 넣어준다
-    pickedCards.forEach((pickedCardType) => {
-      let count = handCards.get(pickedCardType);
-      count = !!count ? count : 0;
-      handCards.set(pickedCardType, ++count);
+    pickedCards.forEach((pickedCard) => {
+      character.addCard(pickedCard)
     });
-    
   };
