7d33aafe6aafd449771f5f9cf5f09566bfa59595
liha-engineer <liha.engineer@gmail.com>
2024-11-25 19:47:29 +0900

    refactor: 유저 업데이트 노티용 데이터(타인) 수정중

--- src/utils/notification/userDatas.js
+++ src/utils/notification/userDatas.js
@@ -1,10 +1,23 @@
 export const parseUserData = (user) => {
-  user.roleType = 0;
-  user.weapon = null;
-  user.stateInfo = null;
-  user.debuffs = [];
-  user.handCards = [];
-  user.bbangCount = 0;
+  const character = user.character;
+  const userDatas = [];
 
-  return user;
+  const parsedUserData = {
+    id: user.id,
+    nickname: user.nickname,
+    character : {
+      roleType : character.roleType === 1 ? character.roleType : 0,
+      hp: character.hp,
+      weapon: 0,
+      stateInfo : character.stateInfo,
+      equips: [],
+      debuffs : character.debuffs,
+      handCards : [], // 얘는 서버상에서 Map 객체이므로 빈 배열로 바꿔 보내줘야 할것 
+      bbangCount : 0,
+      handCardsCount: character.handCardsCount
+    }
+  }
+  userData.push(...parsedUserData)
+  return userDatas;
 };
+
