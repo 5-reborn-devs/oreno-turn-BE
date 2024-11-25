e73b4526f2e2f2feef498e250441025cfd6088ff
liha-engineer <liha.engineer@gmail.com>
2024-11-25 12:50:09 +0900

    refactor: handCards Map 객체로 재수정 (튜터님 피드백)

--- src/classes/models/character.class.js
+++ src/classes/models/character.class.js
@@ -11,7 +11,7 @@ class Character {
     this.stateInfo = new CharacterState();
     this.equips = [];
     this.debuffs = [];
-    this.handCards = [];
+    this.handCards = new Map();
     this.bbangCount = 0;
     this.handCardsCount = 0;
   }
