1685072453743958a5a773bd5d5e4488139ea3e4
yeeunsy <90146746+yeeunsy@users.noreply.github.com>
2024-12-02 21:27:13 +0900

    add: 게임 세션 파일 구조

--- src/events/onError.js
+++ src/events/onError.js
@@ -4,5 +4,8 @@ export const onError = (socket) => (err) => {
   console.error('소켓 오류:', err);
 
   // 오류가 난 유저의 세션을 제거한다.
-  removeUser(socket);
+  // removeUser(socket);
+
+  // 게임 세션을 따로 관리해서 거기에 해당하는 유저를 지워주는 것 같다.
+
 };
--- src/session/session.js
+++ src/session/session.js
@@ -6,4 +6,6 @@ export const users = new Map();
 
 export const roomIdSet = new Set(); // 사용가능한 roomId
 
+//export const gameSessions = new Map();
+
 export let nextRoomId = 1;
