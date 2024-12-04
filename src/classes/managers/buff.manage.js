60421a02b1d6e6e63c63a5096c07b4a73b338c2b
skybanana <112135946+skybanana@users.noreply.github.com>
2024-12-04 18:00:34 +0900

    fix: card.destroy, character buff

--- src/classes/managers/buff.manage.js
+++ src/classes/managers/buff.manage.js
@@ -50,46 +50,6 @@ class BuffManager {
   clearBuff() {
     this.buff.clear();
   }
-
-  // 버프를 통한 공격 계산 처리
-  calculateDamage(damage, targetUser) {
-    const targetBuffs = targetUser.character.buffs;
-
-    // 데미지 영향 버프
-    const POWER = this.getBuff(BUFF_TYPES.POWER);
-    const ARMOR = targetBuffs.getBuff(BUFF_TYPES.ARMOR);
-    const isVulnerable = this.isBuff(BUFF_TYPES.VULNERABLE);
-    const isWeakened = targetBuffs.isBuff(BUFF_TYPES.WEAKENED);
-
-    // 기초 데미지가 0이하라면 0 반환
-    damage = damage + POWER;
-    if (damage <= 0) return 0;
-
-    // 약화 계산 뒤 소비
-    if (isVulnerable) {
-      damage = Math.floor(damage * 0.75);
-      this.comsumeBuff(BUFF_TYPES.VULNERABLE);
-    }
-
-    // 쇠약 계산 뒤 소비
-    if (isWeakened) {
-      damage = Math.floor(damage * 1.5);
-      targetBuffs.comsumeBuff(BUFF_TYPES.VULNERABLE);
-    }
-
-    // 최종 데미지
-    const finalDamage = ARMOR - damage;
-
-    // 쉴드로 전부 방어한 경우
-    if (finalDamage > 0) {
-      targetBuffs.setBuff(new Buff(BUFF_TYPES.SHIELD, finalDamage));
-      return 0;
-    }
-
-    // 전부 막지 못한 경우
-    targetBuffs.deleteBuff(BUFF_TYPES.ARMOR);
-    return finalDamage;
-  }
 }
 
 export default BuffManager;
--- src/handlers/card/card.destroy.handler.js
+++ src/handlers/card/card.destroy.handler.js
@@ -6,31 +6,17 @@ import { sendResponsePacket } from '../../utils/response/createResponse.js';
 //     repeated CardData destroyCards = 1
 // }
 
-function mappingCards(cards) {
-  const map = new Map();
-  cards.forEach((card) => {
-    const key = `${card.type}-${card.count}`;
-    map.set(key, card);
-  });
-  return map;
-}
-
 export const destroyCardsHandler = async (socket, payload) => {
   const { destroyCards } = payload;
 
   try {
     const user = users.get(socket.token);
     const character = user.character;
-    let handCards = [...character.handCards]; // 패 얕은 복사
-
-    // 카드의 속성을 맵핑
-    const handCardsMap = mappingCards(handCards);
-    const destroyCardsMap = mappingCards(destroyCards);
 
     // 패에서 없애려는 카드를 삭제. 동일한 키를 삭제
-    for (const key of destroyCardsMap.keys()) {
-      handCardsMap.delete(key);
-    }
+    destroyCards.forEach((card) => {
+      character.cards.removeHands(card);
+    });
 
     handCards = [...handCardsMap.values()];
 
