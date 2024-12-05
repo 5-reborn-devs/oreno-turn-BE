f2584db1e2e2c87db40881bda597b4ab28b8c4ce
yeeunsy <90146746+yeeunsy@users.noreply.github.com>
2024-12-05 22:09:58 +0900

    refactor

--- src/handlers/card/index.js
+++ src/handlers/card/index.js
@@ -1,3 +1,5 @@
+import { fyShuffle } from "../../utils/fisherYatesShuffle.js";
+
 // 게임 시작 시 준비되어야 할 카드더미 -> (카드 타입값 * 제한 매수) 한 int 배열을 섞은 것
 export const makeCardDeck = (deckInfo) => {
   const gameDeck = [];
