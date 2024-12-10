4abf6b846438b272c52a4c95df54a66707e3910d
qui2dty <96517530+qui2dty@users.noreply.github.com>
2024-12-10 21:42:36 +0900

    fix: 포지션 패킷 조절 수정

--- src/classes/models/room.class.js
+++ src/classes/models/room.class.js
@@ -50,39 +50,50 @@ class Room {
   positionUpdateOn() { 
     if (this.positionUpdateSwitch === false){ 
       this.positionUpdateSwitch = true; 
-      console.log("똑"); } 
-    else if (this.positionUpdateSwitch === true) 
-      { this.positionUpdateSwitch = false; console.log("딱"); 
+      console.log("포지션 업데이트 킴!"); }  
+    }
 
-      } } 
   positionUpdateInterval() { 
     const room = this; console.log("포지션 인터벌 시작"); 
     this.positionIntervalid = setInterval(function () { 
-      room.positionUpdateOn(); }, 1000); 
+      room.positionUpdateOn(); }, 100); 
       // 함수를 전달 
       } 
   
   
-  button() {
+  button(nextPhaseAt) {
     if (this.isPushed) {
-      this.startCustomInterval();
+      this.startCustomInterval(nextPhaseAt);
       this.positionUpdateInterval();
       this.isPushed = false;
     }
   }
 
-  startCustomInterval() {
+  startCustomInterval(nextPhaseAt) {
     const intervals = [18000, 12000, 18000]; // afternoon, evening, night
     let currentIndex = 0;
     const room = this;
     function runInterval() {
+
       // 다음 인터벌 설정
       currentIndex = (currentIndex + 1) % intervals.length;
-      const nextState = intervals[currentIndex];
-      phaseUpdateNotificationHandler(room, nextState);
-      room.intervalId = setTimeout(runInterval, nextState);
+
+      let drifted = nextPhaseAt - Date.now();
+      console.log("시간값 두개 먼저 보여줘! :",nextPhaseAt,Date.now());
+      console.log("드리프트 값!: ",drifted); // 초기값+페이즈 원론적인 시간에서 실제 시간과 비교해서 차이 구하기
+
+      nextPhaseAt += intervals[currentIndex]; // 원론적인 다음 페이즈 시간 업데이트
+
+      const nextState = intervals[currentIndex] + drifted; // 넥스트 페이즈에 시간차이 게산 더해주기
+
+      phaseUpdateNotificationHandler(room, nextState);  //페이즈 업데이트 핸들러 호출 
+      
+      room.intervalId = setTimeout(runInterval, nextState); //다음 인터벌 설정(next state 보정 처리)
     }
-    this.intervalId = setTimeout(runInterval, intervals[currentIndex]);
+
+    const firstDelay = nextPhaseAt - Date.now();     //첫 인터벌 지연시간 계산
+
+    this.intervalId = setTimeout(runInterval, firstDelay);     //첫 인터벌 시작(보정값) 
   }
 
   stopCustomInterval() {
