80f5a4ffe90b14a0a7a5c6199f54b137bea21a8f
skybanana <112135946+skybanana@users.noreply.github.com>
2024-12-19 01:39:49 +0900

    fix: 시작한 방이 리스트에 보이는 문제 수정

--- LOBBY_SERVER/src/handlers/room/room.getList.handler.js
+++ LOBBY_SERVER/src/handlers/room/room.getList.handler.js
@@ -10,7 +10,7 @@ export const getRoomListHandler = async (socket, payloadData) => {
   try {
     const rooms = await redisManager.rooms.getRooms();
     const getRoomListResponse = {
-      rooms: rooms.filter((room) => room.state != inGameState),
+      rooms: rooms.filter((room) => room.state != String(inGameState)),
     };
 
     sendResponsePacket(socket, PACKET_TYPE.GET_ROOM_LIST_RESPONSE, {
