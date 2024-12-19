f0170d6bb1eb8ad83489ff94bd4b7ab5d1419851
skybanana <112135946+skybanana@users.noreply.github.com>
2024-12-19 17:42:45 +0900

    fix: HAproxy 제외

--- LOBBY_SERVER/src/handlers/room/room.leave.handler.js
+++ LOBBY_SERVER/src/handlers/room/room.leave.handler.js
@@ -84,6 +84,6 @@ export const leaveRoomHandler = async (socket, payloadData) => {
 
   // 현재 위치가 로비서버가 아니라면 로비로 돌아감. ? 필요한가?
   if (true) {
-    serverSwitch(socket, config.server.host, 9000);
+    serverSwitch(socket, config.server.host, 6666);
   }
 };
