7d1d7b006c0b7357267ba09648a468723bbead13
skybanana <112135946+skybanana@users.noreply.github.com>
2024-12-20 11:33:25 +0900

    log: 유저 확인 추가

--- GAME_SERVER/src/handlers/auth/verifyToken.handler.js
+++ GAME_SERVER/src/handlers/auth/verifyToken.handler.js
@@ -19,6 +19,7 @@ export const verifyTokenHandler = async (socket, payload) => {
     const user = await redisManager.users.get(token);
     if (Object.keys(user).length) {
       console.error('[verifyToken] token:', token);
+      console.error('user', user);
       throw new Error('유효하지 않은 토큰');
     }
     console.log('verifyToken user:', user);
