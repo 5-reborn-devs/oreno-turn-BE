5ade80162b273bab0ccf8fd42d0417153a0287aa
LeeSanghyun1212 <gustkd7125@gmail.com>
2024-12-22 15:21:35 +0900

    fix
    fix

--- LOBBY_SERVER/src/handlers/sync/heartBeat.handler.js
+++ LOBBY_SERVER/src/handlers/sync/heartBeat.handler.js
@@ -7,6 +7,7 @@ const PING_TIMEOUT = 10000; // 타임아웃 시간 10초 (10000ms)
 export const heartBeatHandler = async (socket, payload) => {
   try {
     const { message, timestamp } = payload;
+    const user = await redisManager.users.get(socket.token);
     const combinedTimestamp = timestamp.toNumber();
     const localTime = Date.now();
     const timeDifference = localTime - combinedTimestamp;
@@ -14,7 +15,7 @@ export const heartBeatHandler = async (socket, payload) => {
     console.log(`유저 ${user.id} - Ping 수신 시간: ${combinedTimestamp}`);
     console.log(`현재 시간: ${localTime}, 시간 차이: ${timeDifference}ms`);
     // 유저 정보 들고옴
-    const user = await redisManager.users.get(socket.token);
+
     console.log('하트비트에서 유저', user);
     // 유저의 핑 시간 업데이트 및 타이머 리셋
     user.updatePingTime(socket);
