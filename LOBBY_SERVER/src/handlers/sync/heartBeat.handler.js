703cef2d4bffc274cbe13242f64a4fb5ea9b61ef
LeeSanghyun1212 <gustkd7125@gmail.com>
2024-12-22 16:41:01 +0900

    fix : socket.destroy()삭제
    fix : socket.destroy()삭제

--- LOBBY_SERVER/src/handlers/sync/heartBeat.handler.js
+++ LOBBY_SERVER/src/handlers/sync/heartBeat.handler.js
@@ -1,7 +1,7 @@
 import { PACKET_TYPE } from '../../constants/header.js';
 import sendResponsePacket from '../../utils/response/createResponse.js';
 
-const PING_TIMEOUT = 10000; // 타임아웃 시간 1초 (1000ms)
+const PING_TIMEOUT = 10000; // 타임아웃 시간 10초 (1000ms)
 let lastPingTime; // 마지막 ping 시간을 기록
 
 // 클라이언트 연결이 끊어진다고 가정한 타이머
@@ -63,7 +63,6 @@ const resetPingTimeout = (socket) => {
 
   socket.setTimeout(PING_TIMEOUT, () => {
     socket.end(); // 타임아웃 시 연결 종료
-    socket.destroy();
     console.log(
       '소켓 타임아웃: 클라이언트 응답이 없습니다. 연결을 종료합니다.',
     );
