d569838af24f2b42538d7016886aaadced9b2c3a
skybanana <112135946+skybanana@users.noreply.github.com>
2025-05-13 01:13:55 +0900

    feat: Nest Lobby Server
    Nest로 로비 API 서버를 구현하기 시작

--- LOBBY_SERVER/src/handlers/room/room.create.handler.js
+++ LOBBY_SERVER/src/handlers/room/room.create.handler.js
@@ -68,7 +68,7 @@ export const createRoomHandler = async (socket, payloadData) => {
   // 게임 서버 리스트를 받음.
   // 라운드로빈으로선정
   // 레디스에 해당 방의 IP를 저장.
-  //if (success) serverSwitch(socket, '127.0.0.1', 16666);
+  if (success) serverSwitch(socket, '127.0.0.1', 16666);
   //   setTimeout(async () => {
   //     if (success) {
   //       serverSwitch(socket, config.server.host, 1666);
